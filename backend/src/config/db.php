e6bf322d92f4d3a129561ea4a0a5d4dc1cb2ce5c
mohamedzait20003 <21-101153@students,eui.edu.eg>
2024-07-23 13:54:31 +0300

    Db.php modified

--- backend/src/config/db.php
+++ backend/src/config/db.php
@@ -1,17 +1,22 @@
 <?php
 namespace App\Config;
+
 use Dotenv\Dotenv;
+use mysqli;
 
 class DB {
     private static $conn = null;
 
     public function __construct() {
         if (self::$conn === null) {
-            $host = 'localhost';
-            $user = 'root';
-            $pass = 'Adsbc1234567@';
-            $name = 'ecommercetest';
-            $port = '3306';
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+            $dotenv->load();
+
+            $host = $_ENV['DB_HOST'];
+            $user = $_ENV['DB_USER'];
+            $pass = $_ENV['DB_PASS'];
+            $name = $_ENV['DB_NAME'];
+            $port = $_ENV['DB_PORT'];
 
             self::$conn = new mysqli($host, $user, $pass, $name, $port);
             if (self::$conn->connect_error) {
