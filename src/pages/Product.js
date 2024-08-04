3baa93cfb261764443bf263bf8381aad9573cbc8
mohamedzait20003 <21-101153@students,eui.edu.eg>
2024-08-04 04:27:29 +0300

    Product Page Finished

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,56 +1,88 @@
 // Libraries
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useLocation } from 'react-router-dom';
 import parse from 'html-react-parser';
 
 const Product = () => {
   const location = useLocation();
   const { product } = location.state || {};
-  const { name, gallery, inStock, price } = product;
-  console.log(gallery);
-
+  const { name, gallery, inStock, AttributeSets, price } = product;
+  
   const [current, setCurrent] = useState(0);
-
   const handlePrevImage = () => {
     setCurrent((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : gallery.length - 1));
   };
-
   const handleNextImage = () => {
     setCurrent((prevIndex) => (prevIndex < gallery.length - 1 ? prevIndex + 1 : 0));
   };
+  
+  const [selectedAttributes, setSelectedAttributes] = useState({});
+  const handleChoseAttribute = (setId, itemId) => {
+    setSelectedAttributes((prev) => ({...prev, [setId]: itemId}));
+  };
+
+  const [AllSelected, setAllSelected] = useState(false);
+  useEffect(() => {
+    if (AttributeSets) {
+      setAllSelected(AttributeSets.every(set => selectedAttributes[set.Id]));
+    }
+    else{
+      setAllSelected(true);
+    }
+  }, [selectedAttributes, AttributeSets]);
 
   return (
     <div className='w-full h-[40rem] p-6'>
       <div className='container h-full flex flex-row'>
         <div className='w-4/6 h-full flex flex-row'>
-          <div className="w-1/4 flex flex-col gap-5 max-h-[40rem] mr-2 overflow-hidden hover:overflow-y-auto">
-            {gallery.map((image, index) => (
-              <img 
-                key={index} 
-                src={image.image_url} 
-                alt={`${name} ${index + 1}`} 
-                className="w-50 mb-2 cursor-pointer" 
-                onClick={() => setCurrent(index)} 
-              />
-            ))}
-          </div>
+          { gallery.length > 1 ? (
+              <div className="w-1/4 flex flex-col gap-5 max-h-[40rem] mr-2 overflow-hidden hover:overflow-y-auto">
+                {gallery.map((image, index) => (
+                  <img key={index} src={image.image_url} alt={`${name} ${index + 1}`} className="w-50 mb-2 cursor-pointer" onClick={() => setCurrent(index)} 
+                  />
+                ))}
+            </div>
+            )
+            : null
+          }
           <div className="relative max-h-[40rem] w-2/3 flex items-start">
-            <button onClick={handlePrevImage} className="absolute left-5 top-1/2 transform -translate-y-1/2 p-2 bg-gray-600 rounded">
+            <button onClick={handlePrevImage} className={`${gallery.length > 1 ? 'absolute' : 'hidden'} left-5 top-1/2 transform -translate-y-1/2 p-2 bg-gray-600 text-white rounded`}>
               &lt;
             </button>
-            <img src={gallery[current].image_url} alt={`${name} ${current + 1}`} className="h-full w-full" />
-            <button onClick={handleNextImage} className="absolute right-5 top-1/2 transform -translate-y-1/2 p-2 bg-gray-600 rounded">
+            <img src={gallery[current].image_url} alt={`${name} ${current + 1}`} className="h-full w-full py-0 px-6" />
+            <button onClick={handleNextImage} className={`${gallery.length > 1 ? 'absolute' : 'hidden'} right-5 top-1/2 transform -translate-y-1/2 p-2 bg-gray-600 text-white rounded`}>
               &gt;
             </button>
           </div>
         </div>
-        <div className='w-2/6 h-full flex flex-col gap-12 mt-10 ml-5'>
-          <h1 className='text-2xl text-slate-800 font-semibold'>{name}</h1>
-          <div className='flex flex-col gap-5 text-xl text-slate-700 font-medium'>
-            <h2>Price:</h2>
-            <p>{price.currency_symbol}{price.amount}</p>
+        <div className='w-2/6 h-full flex flex-col gap-8 mt-5 ml-5'>
+          <h2 className='text-3xl text-slate-800 font-bold'>{name}</h2>
+          <div className='w-2/3 flex flex-col gap-8 '>
+            {
+              AttributeSets.map((attribute, index) => (
+                <div key={index} className='flex flex-col gap-2'>
+                  <h3 className='text-xl text-slate-800 font-semibold'>{attribute.name}:</h3>
+                  <div className='flex flex-row gap-5 mt-4'>
+                    {
+                      attribute.Items.map((item) => (
+                        <button key={item.id} onClick={() => {handleChoseAttribute(attribute.Id, item.id)}}  className={`p-4 rounded-sm ${selectedAttributes[attribute.Id] === item.id ? (attribute.type === 'text' ? 'bg-black text-white' : 'border-2 border-green-500') : ''} ${attribute.type === 'swatch' ? 'swatch' : ''}`} style={attribute.type === 'swatch' ? { '--swatch-color': item.value } : {}} >
+                          {attribute.type === 'text' ? item.value : ''}
+                        </button>
+                      ))
+                    }
+                  </div>
+                </div>
+              ))
+            }
+
           </div>
-          <button className={`bg-green-400 mr-10 p-5 rounded-sm text-white ${inStock ? '' : ''}`}>Add To Cart</button>
+          <div className='flex flex-col gap-5'>
+            <h3 className='text-3xl text-slate-800 font-semibold'>Price:</h3>
+            <p className='text-xl text-slate-700 font-medium'> {price.currency_symbol}{price.amount}</p>
+          </div>
+            <button className={`bg-green-400 mr-10 mt-5 p-5 rounded-sm text-white ${inStock && AllSelected ? '' : 'opacity-50 cursor-not-allowed'}`} >
+              Add To Cart
+            </button>
           <div>
             {parse(product.description)}
           </div>
