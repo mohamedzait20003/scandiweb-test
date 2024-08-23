3d4f2919d6d75f20e07ccb2c34755b1fbc8b66a5
mohamedzait20003 <21-101153@students,eui.edu.eg>
2024-08-23 07:49:09 +0300

    Modified Cart Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // Libraries
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -11,22 +11,14 @@ import Logo from '../assets/Logo/Logo.png';
 
 const Header = ({ categories }) => {
   const [state, setState] = useState(0);
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
   const location = useLocation();
   
   const { totalCount } = useSelector((state) => state.cart);
-
   const dispatch = useDispatch();
-  const handleOpenCart = useCallback((state) => {
-    dispatch(toogleCart(state));
-  }, [dispatch]);
 
-  useEffect(() => {
-    if (isCartOpen !== false) {
-      handleOpenCart(isCartOpen);
-    }
-  }, [isCartOpen, handleOpenCart]);
+  const handleCart = ()  => {
+    dispatch(toogleCart(state));
+  };
 
   useEffect(() => {
     if (location.pathname === `/`) {
@@ -63,7 +55,7 @@ const Header = ({ categories }) => {
         </div>
         <div className='flex items-center gap-5 mr-8'>
           <div className='relative'>
-            <button data-testid='cart-btn' className='h-10 min-w-[50px] bg-transparent flex items-center justify-center rounded-full text-2xl text-blue-950' onClick={() => setIsCartOpen(!isCartOpen)}>
+            <button data-testid='cart-btn' className='h-10 min-w-[50px] bg-transparent flex items-center justify-center rounded-full text-2xl text-blue-950' onClick={handleCart}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
